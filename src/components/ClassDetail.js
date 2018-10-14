9aeb2e7d53d73f7ee12b8fe7fa4a6cd74802b143
Will Htun <32882332+willhtun@users.noreply.github.com>
2018-10-14 01:26:17 -0700

    please work

---
+++ src/components/ClassDetail.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import '@src/theme.css'
+
+export default class ClassDetail extends Component {
+    constructor() {
+        super();
+        this.state = {
+            people: [ 'Felix','Nina','Will' ],
+        }
+    }
+    buildOptions() {
+        var arr = [];
+
+        for (let i = 0; i <= this.state.people.length - 1; i++) {
+            arr.push(<li key={i} value="{i}">{this.state.people[i]}</li>)
+        }
+
+        return arr; 
+    }
+	render(){
+		return(
+            <div className="second-part">           
+                <div className="welcome-message"> {this.props.courseID} </div>
+                <div className="bottom-welcome-message"> These are your classmates in {this.props.courseID}! </div>
+                <ul>
+                    {this.buildOptions()}
+                </ul>
+            </div>
+
+        );
+    }   
+
+}
